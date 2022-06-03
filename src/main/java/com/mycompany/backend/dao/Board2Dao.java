f390e018d24719b1ddec49b583155f5a468ed261
SukheeChoi <95064440+SukheeChoi@users.noreply.github.com>
2022-06-03 11:06:21 +0900

    .

--- src/main/java/com/mycompany/backend/dao/Board2Dao.java
+++ src/main/java/com/mycompany/backend/dao/Board2Dao.java
@@ -1,12 +1,6 @@
 package com.mycompany.backend.dao;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
-
-import com.mycompany.backend.dto.Board;
-import com.mycompany.backend.dto.Board2;
-import com.mycompany.backend.dto.Pager;
 @Mapper
 public interface Board2Dao {
 //  public List<Board2> selectByPage(Pager pager);
