d6595be95048e954ace2a897bdfa0b5248bf6cf3
SukheeChoi <95064440+SukheeChoi@users.noreply.github.com>
2022-06-03 11:06:07 +0900

    delete메소드 Board2Dao에 작성.

--- src/main/java/com/mycompany/backend/dao/Board2Dao.java
+++ src/main/java/com/mycompany/backend/dao/Board2Dao.java
@@ -13,7 +13,7 @@ public interface Board2Dao {
 //  public int count();
 //  public Board selectByBno(int bno);
 //  public int insert(Board board);
-//  public int deleteByBno(int bno);
+  public int deleteByBno(int bno);
 //  public int update(Board board);
 //  public int updateBhitcount(int bno);
 }
